99cd49bec68d41870431d787e67a1bca3e35daca
kdaya <kdayathegamer@gmail.com>
2018-11-22 09:22:10 +1100

    add a 10% increase in colour to black on each pass of square

--- script.js
+++ script.js
@@ -16,10 +16,13 @@ function createGrid(dimension) {
 
 function doHover() {
   let gridItems = document.querySelectorAll(".grid-item");
-  let color = changeColor();
+  // let color = changeColor();
   for(let i=0; i < gridItems.length; i++) {
     gridItems[i].addEventListener("mouseover", () => {
-      gridItems[i].style.backgroundColor = color;
+      // gridItems[i].style.backgroundColor = color;
+      let colour = (shadesOfBlack(gridItems[i].style.backgroundColor));
+      gridItems[i].style.backgroundColor = "rgb(" + colour + "," + colour + "," + colour + ")";
+
     });
   }
 }
@@ -36,10 +39,27 @@ function reset() {
   });
 }
 
-function changeColor() {
-  var num = Math.round(0xffffff * Math.random());
-  var r = num >> 16;
-  var g = num >> 8 & 255;
-  var b = num & 255;
-  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+function shadesOfBlack(currentColour) {
+  let colour = 250 / 10;
+  let currValue = currentColour.substring(
+    currentColour.lastIndexOf(",") + 1,
+    currentColour.lastIndexOf(")")
+  );
+  
+  if (currValue == 0) {
+    return 251;
+  } else if (currValue < 254 && currValue > 1) {
+    return currValue - colour;
+  } else if (currValue < 5) {
+    return 1;
+  }
 }
+
+//Change hover to a random colour
+// function changeColor() {
+//   var num = Math.round(0xffffff * Math.random());
+//   var r = num >> 16;
+//   var g = num >> 8 & 255;
+//   var b = num & 255;
+//   return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+// }
