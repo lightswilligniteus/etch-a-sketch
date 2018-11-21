cee3eecf064035986ce91a8153873616d878df78
kdaya <kdayathegamer@gmail.com>
2018-11-22 08:11:09 +1100

    fixed lag by removing innerHTML

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ reset();
 function createGrid(dimension) {
   let container = document.querySelector(".container");
   for(let j=0; j < dimension*dimension; j++) {
-    container.innerHTML += "<div class='grid-item'></div>";
+    let div = document.createElement('div');
+    div.classList.add('grid-item');
+    container.appendChild(div);
   }
   let gridTemplate = "grid-template-columns:repeat(" + dimension + ", auto)";
   container.setAttribute('style', gridTemplate);
