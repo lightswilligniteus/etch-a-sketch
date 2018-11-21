677af3a3883b96a610432a7ee39ef30a781f898d
Karishma <kdayathegamer@gmail.com>
2018-11-21 19:46:46 +1100

    grid template created with grid display

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-createGrid();
+createGrid(16);
 
-function createGrid() {
+function createGrid(dimension) {
   let container = document.querySelector(".container");
-  for(let i=0; i < 16; i++) {
-    container.innerHTML += "<div>";
-    for(let j=0; j < 16; j++) {
-      container.innerHTML += "<div class='innerDiv'></div>";
-    }
-    container.innerHTML += "</div>"
+  for(let j=0; j < dimension*dimension; j++) {
+    container.innerHTML += "<div class='grid-item'></div>";
   }
+  let gridTemplate = "grid-template-columns:repeat(" + dimension + ", auto)";
+  container.setAttribute('style', gridTemplate);
 }
