8025857724c923c200811bc1983e1917e8228914
Karishma <kdayathegamer@gmail.com>
2018-11-21 20:05:51 +1100

    fill in grid on hover

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 createGrid(16);
+doHover();
 
 function createGrid(dimension) {
   let container = document.querySelector(".container");
@@ -7,4 +8,14 @@ function createGrid(dimension) {
   }
   let gridTemplate = "grid-template-columns:repeat(" + dimension + ", auto)";
   container.setAttribute('style', gridTemplate);
+}
+
+function doHover() {
+  let gridItems = document.querySelectorAll(".grid-item");
+  console.log(gridItems.length);
+  for(let i=0; i < gridItems.length; i++) {
+    gridItems[i].addEventListener("mouseover", () => {
+      gridItems[i].setAttribute("class", "colored");
+    });
+  }
 }
